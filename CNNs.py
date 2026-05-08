5929e0c3d307e3961c89e275a0c94ab1de038c21
Sagacity12 <esaga5688@gmail.com>
2026-05-08 23:42:18 +0000

    Convolutional Neurral Network

---
+++ CNNs.py
@@ -0,0 +1,58 @@
+import os
+
+from classification_model_keras import X_train
+
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Input
+from keras.utils import to_categorical
+from keras.datasets import mnist
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+
+# Convolutional Neural Network with One Set of Convolutional and Pooling Layers
+# load Data
+(x_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# reshape to be [samples][pixels][height][width]
+X_train = X_train.reshape(X_train.shape[0], 28, 28, 1).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1).astype('float32')
+
+# normalizing train and text data
+X_train = X_train / 255
+X_test = X_test / 255
+
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+# number of categories
+num_classes = y_test.shape[1]
+
+# function to create a model
+def convolutional_model():
+    model = Sequential()
+    model.add(Input(shape=(28, 28, 1)))
+    model.add(Conv2D(16, (5,5), strides=(2,2), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    # compile model
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+# build the model
+model = convolutional_model()
+
+# fit the model
+model.fit(X_train, y_train, batch_size=32, epochs=10, verbose=1, validation_data=(X_test, y_test))
+
+# evaluate the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: {}\n Error: {}".format(scores[1], 100-scores[1]*100))
